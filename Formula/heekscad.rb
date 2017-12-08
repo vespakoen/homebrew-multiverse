cbd894cde2d9d7b3a63dcbde4e7138ee5c2fea1d
mtlott <mtlott@users.noreply.github.com>
2017-12-08 07:50:48 -0500

    heekscad 1.0.1 (new formula)

---
+++ Formula/heekscad.rb
@@ -0,0 +1,21 @@
+class Heekscad < Formula
+  desc "HeeksCAD is an open source, CAD application based on OpenCASCADE"
+  homepage "https://github.com/mtlott/heekscad"
+  url "https://github.com/mtlott/heekscad.git"
+  version "1.0.1"
+  sha256 ""
+
+  depends_on "cmake" => :build
+  depends_on "science/oce"
+  depends_on "mtlott/multiverse/libarea"
+  depends_on "wxmac"
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system "true"
+  end
+end
